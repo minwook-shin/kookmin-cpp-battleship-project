c3a2241ca666c5521a9143b607d23c0d588da975
minwook-shin <minwook0106@gmail.com>
2018-05-18 17:48:40 +0900

    add ship class code, random code

--- main.cpp
+++ main.cpp
@@ -2,7 +2,8 @@
 #include <cstring>
 #include <vector>
 #include <iostream>
-#include <stdlib.h>
+#include <cstdlib>
+#include <set>
 #define X 10
 #define Y 10
 
@@ -25,7 +26,7 @@ public:
   void setUi()
   {
     init_pair(1, COLOR_WHITE, COLOR_BLACK);
-    init_pair(2, COLOR_GREEN, COLOR_BLUE);
+    init_pair(2, COLOR_GREEN, COLOR_WHITE);
     init_pair(3, COLOR_CYAN, COLOR_BLACK);
     init_pair(4, COLOR_YELLOW, COLOR_BLACK);
     border('*', '*', '*', '*', '*', '*', '*', '*');
@@ -155,7 +156,155 @@ public:
   WINDOW *getWin() { return inputWIn; }
 };
 
-class gameManager
+class ship
+{
+private:
+  vector<int> randomA;
+  vector<int> randomB;
+  vector<int> randomC;
+  vector<int> randomD;
+  vector<int> random0;
+
+  set<int> numCheck;
+
+public:
+  ship(/* args */)
+  {
+    srand(time(0));
+    genValue();
+  }
+  ~ship() {}
+  int getA(int i)
+  {
+    return randomA[i];
+  }
+  int getB(int i)
+  {
+    return randomB[i];
+  }
+  int getC(int i)
+  {
+    return randomC[i];
+  }
+  int getD(int i)
+  {
+    return randomD[i];
+  }
+  int genArrow()
+  {
+    random0.push_back(1 + rand() % 2);
+    return random0[0];
+  }
+  void genValue()
+  {
+    while (numCheck.size() != 5)
+    {
+      numCheck.clear();
+      randomA.clear();
+      randomB.clear();
+      randomC.clear();
+      randomD.clear();
+      randomA.push_back(1 + rand() % 8);
+      randomB.push_back(1 + rand() % 8);
+      randomC.push_back(1 + rand() % 8);
+      randomD.push_back(1 + rand() % 8);
+      randomD.push_back(1 + rand() % 8);
+
+      numCheck.insert(randomA[0]);
+      numCheck.insert(randomB[0]);
+      numCheck.insert(randomC[0]);
+      numCheck.insert(randomD[0]);
+      numCheck.insert(randomD[1]);
+    }
+    randomA.push_back(1 + rand() % 4);
+    randomB.push_back(1 + rand() % 5);
+    randomC.push_back(1 + rand() % 6);
+    randomD.push_back(1 + rand() % 7);
+    randomD.push_back(1 + rand() % 7);
+  }
+
+  void drawShip(int **(&t), WINDOW *w, WINDOW *c)
+  {
+    if (genArrow() == 1)
+      for (int i = 0; i < X; i++)
+      {
+        for (int j = 0; j < Y; j++)
+        {
+          if (i == randomA[0] && j == randomA[1])
+          {
+            t[i][j] = 2;
+            t[i][j + 1] = 2;
+            t[i][j + 2] = 2;
+            t[i][j + 3] = 2;
+            t[i][j + 4] = 2;
+          }
+          if (i == randomB[0] && j == randomB[1])
+          {
+            t[i][j] = 3;
+            t[i][j + 1] = 3;
+            t[i][j + 2] = 3;
+            t[i][j + 3] = 3;
+          }
+          if (i == randomC[0] && j == randomC[1])
+          {
+            t[i][j] = 4;
+            t[i][j + 1] = 4;
+            t[i][j + 2] = 4;
+          }
+          if (i == randomD[0] && j == randomD[2])
+          {
+            t[i][j] = 5;
+            t[i][j + 1] = 5;
+          }
+          if (i == randomD[1] && j == randomD[3])
+          {
+            t[i][j] = 5;
+            t[i][j + 1] = 5;
+          }
+        }
+      }
+    if (genArrow() == 2)
+      for (int i = 0; i < X; i++)
+      {
+        for (int j = 0; j < Y; j++)
+        {
+          if (j == randomA[0] && i == randomA[1])
+          {
+            t[i][j] = 2;
+            t[i + 1][j] = 2;
+            t[i + 2][j] = 2;
+            t[i + 3][j] = 2;
+            t[i + 4][j] = 2;
+          }
+          if (j == randomB[0] && i == randomB[1])
+          {
+            t[i][j] = 3;
+            t[i + 1][j] = 3;
+            t[i + 2][j] = 3;
+            t[i + 3][j] = 3;
+          }
+          if (j == randomC[0] && i == randomC[1])
+          {
+            t[i][j] = 4;
+            t[i + 1][j] = 4;
+            t[i + 2][j] = 4;
+          }
+          if (j == randomD[0] && i == randomD[2])
+          {
+            t[i][j] = 5;
+            t[i+1][j] = 5;
+          }
+          if (j == randomD[1] && i == randomD[3])
+          {
+            t[i][j] = 5;
+            t[i+1][j] = 5;
+          }
+        }
+      }
+  }
+};
+
+class gameManager : public ship
 {
 private:
   int turn;
@@ -212,6 +361,7 @@ public:
           t[i][j] = 1;
         }
       }
+      drawShip(t, w, c);
     }
   }
 
@@ -249,35 +399,66 @@ public:
   void updateAttackmap(WINDOW *w, char *input, WINDOW *c)
   { // AAAAA : 2 , BBBB : 3 , CCC : 4 , DD : 5
     int tmp = atoi(input);
-    t[tmp / 10][tmp % 10] = 6;
-    
+    for (int i = 0; i < X; i++)
+    {
+      for (int j = 0; j < Y; j++)
+      {
+        if (t[tmp / 10][tmp % 10] == 0)
+        {
+          t[tmp / 10][tmp % 10] = 6;
+        }
+        if (t[tmp / 10][tmp % 10] == 2)
+        {
+          t[tmp / 10][tmp % 10] = 7;
+        }
+        if (t[tmp / 10][tmp % 10] == 3)
+        {
+          t[tmp / 10][tmp % 10] = 7;
+        }
+        if (t[tmp / 10][tmp % 10] == 4)
+        {
+          t[tmp / 10][tmp % 10] = 7;
+        }
+        if (t[tmp / 10][tmp % 10] == 5)
+        {
+          t[tmp / 10][tmp % 10] = 7;
+        }
+      }
+    }
+
     for (int i = 0; i < X; i++)
     {
       for (int j = 0; j < Y; j++)
       {
         if (t[i][j] == 0)
         {
-          mvwprintw(w, i * 2 - 1, j * 2 - 1, "0");
+          mvwprintw(w, i * 2 - 1, j * 2 - 1, "O");
         }
         if (t[i][j] == 2)
         {
-          mvwprintw(w, i * 2 - 1, j * 2 - 1, "0");
+          mvwprintw(w, i * 2 - 1, j * 2 - 1, "O");
         }
         if (t[i][j] == 3)
         {
-          mvwprintw(w, i * 2 - 1, j * 2 - 1, "0");
+          mvwprintw(w, i * 2 - 1, j * 2 - 1, "O");
         }
         if (t[i][j] == 4)
         {
-          mvwprintw(w, i * 2 - 1, j * 2 - 1, "0");
+          mvwprintw(w, i * 2 - 1, j * 2 - 1, "O");
         }
         if (t[i][j] == 5)
         {
-          mvwprintw(w, i * 2 - 1, j * 2 - 1, "0");
+          mvwprintw(w, i * 2 - 1, j * 2 - 1, "O");
         }
         if (t[i][j] == 6)
         {
+          mvwprintw(w, i * 2 - 1, j * 2 - 1, "M");
+          mvwprintw(c, 5, 1, "Miss!");
+        }
+        if (t[i][j] == 7)
+        {
           mvwprintw(w, i * 2 - 1, j * 2 - 1, "H");
+          mvwprintw(c, 5, 1, "Hit!!");
         }
       }
     }
@@ -295,7 +476,7 @@ private:
 public:
   player(/* args */) {}
   ~player() {}
-  void inputPlayer(WINDOW *DW, WINDOW *AW, WINDOW *SW, WINDOW *IW, char *(&input))
+  void inputPlayer(WINDOW *DW, WINDOW *AW, WINDOW *SW, WINDOW *IW, char *(&input), int **(&t))
   {
     wgetnstr(IW, in, 2);
 
@@ -305,11 +486,6 @@ public:
       {
 
         mvwprintw(SW, 7, 14, in);
-        addTurn();
-        sprintf(turnStr, "%d", getTurn());
-        mvwprintw(SW, 2, 8, turnStr);
-        wrefresh(SW);
-        wmove(IW, 2, 9);
 
         if (in[0] == 'A')
         {
@@ -343,6 +519,16 @@ public:
         {
           in[0] = '8';
         }
+        int tmp = atoi(in);
+        if (t[tmp / 10][tmp % 10] != 6 && t[tmp / 10][tmp % 10] != 7)
+        {
+          addTurn();
+        }
+
+        sprintf(turnStr, "%d", getTurn());
+        mvwprintw(SW, 2, 8, turnStr);
+        wrefresh(SW);
+        wmove(IW, 2, 9);
 
         input = in;
       }
@@ -356,15 +542,6 @@ public:
       wmove(IW, 2, 9);
     }
   }
-};
-
-class ship
-{
-private:
-  /* data */
-public:
-  ship(/* args */) {}
-  ~ship() {}
 };
 
 int main()
@@ -387,7 +564,7 @@ int main()
   {
     gm.updateDefendmap(dw.getWin(), iw.getWin());
     gm.updateAttackmap(aw.getWin(), gm.in, iw.getWin());
-    a.inputPlayer(dw.getWin(), aw.getWin(), sw.getWin(), iw.getWin(), gm.in);
+    a.inputPlayer(dw.getWin(), aw.getWin(), sw.getWin(), iw.getWin(), gm.in, gm.t);
   }
 
   getch();
