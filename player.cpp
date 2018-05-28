6523facda703ffd9c1121481b29fce2c7bae1e27
minwook-shin <minwook0106@gmail.com>
2018-05-28 21:34:50 +0900

    add init random ai

--- player.cpp
+++ player.cpp
@@ -1,14 +1,84 @@
-
+#include <unistd.h>
 #include "player.h"
 
 using namespace std;
 
-player::player(/* args */) {}
+player::player(/* args */):aiInput(new char[2])
+{
+  srand(time(0));
+}
 player::~player() {}
+
+char* player::ai()
+{
+  randomInputX = (1 + rand() % 8);
+  randomInputY = (1 + rand() % 8);
+  if(randomInputX == 1)
+  {
+    aiInput[0] = 'A';
+  }
+  if(randomInputX == 2)
+  {
+    aiInput[0] = 'B';
+  }
+  if(randomInputX == 3)
+  {
+    aiInput[0] = 'C';
+  }
+  if(randomInputX == 4)
+  {
+    aiInput[0] = 'D';
+  }
+  if(randomInputX == 5)
+  {
+    aiInput[0] = 'E';
+  }
+  if(randomInputX == 6)
+  {
+    aiInput[0] = 'F';
+  }
+  if(randomInputX == 7)
+  {
+    aiInput[0] = 'G';
+  }
+  if(randomInputX == 8)
+  {
+    aiInput[0] = 'H';
+  }
+
+  if (randomInputY == 1) {
+    aiInput[1] = '1';
+  }
+  if (randomInputY == 2) {
+    aiInput[1] = '2';
+  }
+  if (randomInputY == 3) {
+    aiInput[1] = '3';
+  }
+  if (randomInputY == 4) {
+    aiInput[1] = '4';
+  }
+  if (randomInputY == 5) {
+    aiInput[1] = '5';
+  }
+  if (randomInputY == 6) {
+    aiInput[1] = '6';
+  }
+  if (randomInputY == 7) {
+    aiInput[1] = '7';
+  }
+  if (randomInputY == 8) {
+    aiInput[1] = '8';
+  }
+  
+  return aiInput;
+}
 
 void player::inputPlayer(WINDOW *DW, WINDOW *AW, WINDOW *SW, WINDOW *IW, char *(&input), int **(&t))
 {
-  wgetnstr(IW, in, 2);
+  // wgetnstr(IW, in, 2);
+  in = ai();
+  sleep(1);
 
   if (toupper(in[0]) == 'A' || toupper(in[0]) == 'B' || toupper(in[0]) == 'C' || toupper(in[0]) == 'D' || toupper(in[0]) == 'E' || toupper(in[0]) == 'F' || toupper(in[0]) == 'G' || toupper(in[0]) == 'H')
   {
@@ -77,3 +147,5 @@ void player::inputPlayer(WINDOW *DW, WINDOW *AW, WINDOW *SW, WINDOW *IW, char *(
     wmove(IW, 2, 9);
   }
 }
+
+
--- player.h
+++ player.h
@@ -4,8 +4,14 @@
 
 class player : public gameManager
 {
+private:
+  int randomInputX;
+  int randomInputY;;
+  char* aiInput;
+
 public:
   player();
   ~player();
   void inputPlayer(WINDOW *DW, WINDOW *AW, WINDOW *SW, WINDOW *IW, char *(&input), int **(&t));
+  char* ai();
 };
